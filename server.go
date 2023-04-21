5566bb9ab4d45fab7d978260ffdde2a999f01c85
anbt <buithean2010@yahoo.com>
2023-04-22 00:10:36 +0900

    commit middlewares

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 	"web-go-gin/controllers"
 	"web-go-gin/services"
 
@@ -13,8 +15,33 @@ var (
 	videoCtl controllers.VideoController = controllers.NewVideoCtl(videoSvc)
 )
 
+func setupLogger(toFile bool) {
+	if toFile {
+		// Disable Console Color, you don't need console color when writing the logs to file.
+		gin.DisableConsoleColor()
+
+		// Logging to a file.
+		f, _ := os.Create("gin.log")
+		gin.DefaultWriter = io.MultiWriter(f)
+
+		// Logging to a file AND console at the same time.
+		//gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+	}
+}
+
 func main() {
-	r := gin.Default()
+	setupLogger(false)
+
+	r := gin.New()
+
+	r.Use(gin.Recovery())
+
+	// r.Use(middlewares.Logger())
+
+	// r.Use(gindump.Dump())
+
+	// r.Use(middlewares.BasicAuth())
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
